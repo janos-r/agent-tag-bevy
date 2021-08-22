e9b4edb889a82ac6f79d03631f0ef0af3741728a
Radim Janoš <radim.janos@apitree.cz>
2021-08-22 21:54:49 +0200

    resources mod, system ordering instead stages ordering

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 mod input;
+mod resources;
 
 use bevy::prelude::*;
-use input::Input;
-use rand::{rngs::SmallRng, Rng, SeedableRng};
+use rand::Rng;
+use resources::*;
 use std::{thread, time::Duration};
-use structopt::StructOpt;
 
 struct Agent;
 
 #[derive(Clone, PartialEq, Debug)]
-enum Status {
+pub enum Status {
     Normal,
     Tagged,
     UnTaggable,
@@ -80,53 +80,17 @@ fn print_grid(grid: Res<Grid>, grid_size: Res<InputSize>, tag_count: Res<TagCoun
     println!("Total count of exchanged tags: {}", tag_count.0)
 }
 
-// Resources
-struct InputAgents(usize);
-struct InputTime(u64);
-struct InputSize(usize);
-struct InputMoves(usize);
-struct InputAnnounceTag(bool);
-struct InputDisableGrid(bool);
-
-#[derive(Default)]
-struct TagCount(u32);
-struct Random(SmallRng);
-struct Grid(Vec<Vec<Option<Status>>>);
-
 fn main() {
-    let Input {
-        agents: n_of_agents,
-        time: sleep_in_millis,
-        size,
-        moves,
-        print_announce: announce_tag,
-        disable_grid,
-    } = Input::from_args();
-
     static UPDATE_GRID: &str = "update_grid";
     static PRINT_GRID: &str = "print_grid";
     static SLEEP: &str = "sleep";
     App::build()
         .add_plugins(MinimalPlugins)
-        .add_stage_after(
-            CoreStage::Update,
-            UPDATE_GRID,
-            SystemStage::single_threaded(),
-        )
-        .add_stage_after(UPDATE_GRID, PRINT_GRID, SystemStage::single_threaded())
-        .add_stage_after(PRINT_GRID, SLEEP, SystemStage::single_threaded())
-        .insert_resource(InputAgents(n_of_agents))
-        .insert_resource(InputTime(sleep_in_millis))
-        .insert_resource(InputSize(size))
-        .insert_resource(InputMoves(moves))
-        .insert_resource(InputAnnounceTag(announce_tag))
-        .insert_resource(InputDisableGrid(disable_grid))
-        .insert_resource(Random(SmallRng::from_entropy()))
-        .insert_resource(Grid(Vec::with_capacity(size)))
-        .init_resource::<TagCount>()
+        .add_plugin(LoadResources)
         .add_startup_system(add_agents.system())
-        .add_system_to_stage(UPDATE_GRID, update_grid.system())
-        .add_system_to_stage(PRINT_GRID, print_grid.system())
-        .add_system_to_stage(SLEEP, sleep2s.system())
+        // Run every tick
+        .add_system(update_grid.system().label(UPDATE_GRID))
+        .add_system(print_grid.system().label(PRINT_GRID).after(UPDATE_GRID))
+        .add_system(sleep2s.system().label(SLEEP).after(PRINT_GRID))
         .run();
 }
---
+++ src/resources.rs
@@ -0,0 +1,41 @@
+use super::input::Input;
+use super::Status;
+use bevy::prelude::*;
+use rand::{rngs::SmallRng, SeedableRng};
+use structopt::StructOpt;
+
+// Resources
+pub struct InputAgents(pub usize);
+pub struct InputTime(pub u64);
+pub struct InputSize(pub usize);
+pub struct InputMoves(pub usize);
+pub struct InputAnnounceTag(pub bool);
+pub struct InputDisableGrid(pub bool);
+
+#[derive(Default)]
+pub struct TagCount(pub u32);
+pub struct Random(pub SmallRng);
+pub struct Grid(pub Vec<Vec<Option<Status>>>);
+
+pub struct LoadResources;
+impl Plugin for LoadResources {
+    fn build(&self, app: &mut AppBuilder) {
+        let Input {
+            agents: n_of_agents,
+            time: sleep_in_millis,
+            size,
+            moves,
+            print_announce: announce_tag,
+            disable_grid,
+        } = Input::from_args();
+        app.insert_resource(InputAgents(n_of_agents))
+            .insert_resource(InputTime(sleep_in_millis))
+            .insert_resource(InputSize(size))
+            .insert_resource(InputMoves(moves))
+            .insert_resource(InputAnnounceTag(announce_tag))
+            .insert_resource(InputDisableGrid(disable_grid))
+            .insert_resource(Random(SmallRng::from_entropy()))
+            .insert_resource(Grid(Vec::with_capacity(size)))
+            .init_resource::<TagCount>();
+    }
+}
